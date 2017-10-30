d01ec87233af7f89681da82e12745edc133a10da
xawotihs <xawotihs@gmail.com>
2017-10-30 08:09:09 +0100

    Clamps weights, removes some pivot bug

--- strategies/universal.py
+++ strategies/universal.py
@@ -16,16 +16,12 @@ class Universal(Base):
     """
     About: Multi-currency strategy focusing on using universal portfolio management algorithms
     """
-    pair_delimiter = None
+    first_values = pd.DataFrame()
 
     def __init__(self):
         super(Universal, self).__init__()
         self.name = 'universal'
-        self.min_history_ticks = 26
-        self.previous_obv = {}
-        self.obv_interval = 3
-        self.previous_macds = {}
-        self.prev_pair_adx = {}
+        self.min_history_ticks = 5
         self.buy_sell_mode = BuySellMode.user_defined
         self.active_pairs = []
         self.algo = algos.OLMAR()
@@ -56,14 +52,22 @@ class Universal(Base):
             amounts[i] = amounts[i] * self.get_price(TradeState.none, look_back, 'BTC_'+currencies[i])
             sum += amounts[i]
 
-        return (np.asarray(amounts) / sum, sum)
+        # let's hide the negative weights from the lib
+        weights = np.asarray(amounts) / sum
+        weights.clip(0,1)
+
+        return (weights, sum)
 
 
     def my_next_weights(self, S, last_b, **kwargs):
         """ Calculate weights for next day. """
         # use history in step method?
         use_history = self.algo._use_history_step()
-        history = self.algo._convert_prices(S, self.algo.PRICE_TYPE)
+
+        history = S / self.first_values
+
+
+        #history = self.algo._convert_prices(S, self.algo.PRICE_TYPE)
         x = history.iloc[-1]
 
         if use_history:
@@ -77,8 +81,6 @@ class Universal(Base):
         """
         Main Strategy function, which takes recent history data and returns recommended list of actions
         """
-        if not self.pair_delimiter:
-            self.pair_delimiter = self.get_delimiter(look_back)
 
         (dataset_cnt, pairs_count) = common.get_dataset_count(look_back, self.group_by_field)
 
@@ -97,13 +99,21 @@ class Universal(Base):
         current_amounts = list(wallet.current_balance.values())
 
         currencies = list(wallet.current_balance.keys())
-        converted_look_back = look_back.pivot(index='date', columns='pair', values='close')
+        look_back.reset_index()
+#        converted_look_back = look_back.pivot(index=['date', 'pair'], columns='pair', values='close')
+        converted_look_back = look_back.pivot_table(index='date', columns='pair', values='close')
 
         converted_look_back['BTC_BTC'] = 1
         converted_look_back = converted_look_back.filter(items = self.compute_relevant_pairs(wallet, 'BTC'))
         converted_look_back = converted_look_back.tail(self.min_history_ticks)
 
+        if self.first_values.empty:
+            self.first_values = converted_look_back.iloc[0]
+            self.algo.init_step(converted_look_back)
+
         next_weights = self.my_next_weights(converted_look_back, current_weights)
+#        next_weights = (current_weights + next_weights) / 2
+#        next_weights = self.algo.next_weights(converted_look_back, current_weights)
 
         # We sell first, then we buy
         # if new_weight < current_weight, we need to sell the related currency
@@ -122,11 +132,13 @@ class Universal(Base):
         # if new_weight > current_weight, we need to buy the related currency
         for i in range(0, len(currencies)):
             if (next_weights[i] > current_weights[i]
-                and currencies[i] != 'BTC'):
-                close_pair_price = self.get_price(TradeState.sell, look_back, 'BTC_'+currencies[i])
+                and currencies[i] != 'BTC'
+                and next_weights[i] != 0):
+                close_pair_price = self.get_price(TradeState.sell, look_back, 'BTC_' + currencies[i])
                 action = TradeAction('BTC_'+currencies[i],
                                      TradeState.buy,
-                                     amount= (wallet_value/close_pair_price) * (next_weights[i]-current_weights[i])/next_weights[i],
+                                     # let's only buy 99% of what the algo recommands to avoid going negative with the fees
+                                     amount= 0.99*(wallet_value/close_pair_price) * (next_weights[i]-current_weights[i]),
                                      rate=close_pair_price,
                                      buy_sell_mode=self.buy_sell_mode)
                 self.actions.append(action)
